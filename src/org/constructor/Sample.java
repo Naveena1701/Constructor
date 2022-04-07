2cdff70c1b01dd03721cd0cf0fa61694ecff8714
Naveena1701 <naveeria@gmail.com>
2022-04-06 17:57:29 -0700

    DOne

--- src/org/constructor/Sample.java
+++ src/org/constructor/Sample.java
@@ -1,5 +1,19 @@
 package org.constructor;
 
 public class Sample {
-
+	public Sample() {
+		
+		System.out.println("Non parametrized Constructor method");
+	}
+	public Sample(int w) {
+		this('o');
+		System.out.println("Parametrized Constructor method-int");
+	}
+	public Sample(char c) {
+		this();
+		System.out.println("Parametrized Constructor method-char");
+	}
+	public static void main(String[] args) {
+		Sample s=new Sample(45);
+	}
 }
